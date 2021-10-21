0215203f60be6c0cceec15358742cb55a1dc7360
r-4v <pravaspraveen777@gmail.com>
2021-10-21 16:41:48 +0530

    removed redundancy

--- app.js
+++ app.js
@@ -1,29 +1,26 @@
-import {actionNodes,domStuff} from './domsetup.js';
-import { dynamicListeners } from './domManipulate.js';
-import {gameBoard,gameController} from './gameLogic.js';
-function startGame()
-{
-//setting up game
-console.log("starting");
-gameBoard.resetGameState();
-// setup over
-//dom setup
-domStuff.resetGrid();
-domStuff.gridItems = document.querySelectorAll(".grid-item");
-//dom manipulate
-gameController.setInitialGameState();
-dynamicListeners.attachListeners();
-
+import { actionNodes, domStuff } from "./domsetup.js";
+import { dynamicListeners } from "./domManipulate.js";
+import { gameBoard, gameController } from "./gameLogic.js";
+function startGame() {
+  //setting up game
+  console.log("starting");
+  gameBoard.resetGameState();
+  // setup over
+  //dom setup
+  domStuff.resetGrid();
+  domStuff.gridItems = document.querySelectorAll(".grid-item");
+  //dom manipulate
+  gameController.setInitialGameState();
+  dynamicListeners.attachListeners();
 }
 startGame();
 
-const resetGame = (function(){
+const resetGame = (function () {
   let resetButton = document.querySelector("#restart-button");
-  resetButton.addEventListener("click",reset);
-  function reset(){
-    console.log('restarting');
+  resetButton.addEventListener("click", reset);
+  function reset() {
+    console.log("restarting");
     startGame();
   }
-  return {reset};
-})()
-
+  return { reset };
+})();
